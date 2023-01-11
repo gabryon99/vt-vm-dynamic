1e3254b4503ffea34acb8fe73e46404d5e5b7813
Gabriele Pappalardo <leleattolo99@gmail.com>
2023-01-11 18:43:30 +0100

    generalizes emulator engine main loop with profiling capabilities

--- src/lib.rs
+++ src/lib.rs
@@ -3,66 +3,46 @@ pub mod memory;
 pub mod program;
 pub mod translation;
 
-use std::process::exit;
-
 use caches::Cache;
 use cpu::{Cpu, OpCode};
-use inkwell::context::Context;
+use log::{debug, warn, info};
 use memory::{Addressable, Memory};
+
+use inkwell::context::Context;
 use program::Program;
-use translation::TranslatorEngine;
+use translation::TranslationContext;
 
 const CACHE_SIZE: usize = 32;
+const MAX_EXECUTIONS: u64 = 1;
 
-type CodeCache<'ctx> = caches::AdaptiveCache<u32, TranslatorEngine<'ctx>>;
+type CodeCache<'ctx> = caches::AdaptiveCache<usize, TranslationContext<'ctx>>;
 
+#[derive(Default)]
 pub struct EmulationEngine {
-    cpu: Cpu,
+    pub(crate) cpu: Cpu,
     memory: Memory,
 }
 
 impl EmulationEngine {
-
-    pub fn new() -> Self {
-        Self {
-            cpu: Cpu::new(),
-            memory: Memory::new(),
-        }
-    }
-
     pub fn load_program(&mut self, program: Program) {
         // Set the initial register values
         self.cpu.acc = program.initial_acc;
         self.cpu.lc = program.initial_lc;
 
         // Load the program in memory
-        match self.memory.write_chunk(program.data) {
-            Ok(()) => (),
-            Err(e) => {
-                log::error!(
-                    "an error occurred when loading the program in memory: {}",
-                    e
-                );
-                exit(-1);
-            }
-        }
+        self.memory
+            .write_chunk(program.data)
+            .expect("Failed to write program into memory!");
     }
 
-    #[inline(always)]
     fn debug_state(&self) {
-        #[cfg(debug_assertions)]
-        {
-            let next_eights = (self.cpu.pc..self.cpu.pc + 8).fold(String::new(), |acc, address| {
-                acc + &format!("{:#04x} ", self.memory.read(address as usize)).to_string()
-            });
-            log::debug!(
-                "State: PC: {:#04x}, ACC: {:#4}, LC: {:#4} | {}",
-                self.cpu.pc,
-                self.cpu.acc,
-                self.cpu.lc,
-                next_eights
-            );
-        }
+        let next_eights = (self.cpu.pc..self.cpu.pc + 8).fold(String::new(), |acc, address| {
+            acc + &format!("{:#04x} ", self.memory.read(address as usize))
+        });
+        debug!(
+            "State: PC: {:#04x}, ACC: {:#4}, LC: {:#4} | {}",
+            self.cpu.pc, self.cpu.acc, self.cpu.lc, next_eights
+        );
     }
 
     fn interpret(&mut self) -> Vec<OpCode> {
@@ -70,12 +50,10 @@ impl EmulationEngine {
         let mut dynamic_block = Vec::new();
 
         loop {
-            let instr: OpCode = self.memory.read(self.cpu.pc as usize).into();
-            dynamic_block.push(instr);
+            let instr = OpCode::try_from(self.memory.read(self.cpu.pc))
+                .expect("Unknown OpCode read from memory.");
 
-            // log::debug!("Executing instruction: {:?}", instr);
-            // log::debug!("PC: {:#04x}, ACC: {:#6}, LC: {:#6}", self.cpu.pc, self.cpu.acc, self.cpu.lc);
-            self.debug_state();
+            dynamic_block.push(instr);
 
             match instr {
                 OpCode::HALT => {
@@ -113,49 +91,164 @@ impl EmulationEngine {
     }
 
     pub fn main_loop(&mut self) {
-
         let llvm_context = Context::create();
         let mut code_cache = CodeCache::new(CACHE_SIZE).unwrap();
 
-        // As long the machine is not halt
+        // As long the machine is not stopped
         while !self.cpu.halt {
-            let pc = self.cpu.pc.clone();
-            let translator = code_cache.get(&pc);
+            let pc = self.cpu.pc;
+            let tbb = code_cache.get_mut(&pc);
 
-            if let Some(translator) = translator {
-                log::debug!(
-                    "found translation block for pc: {}, executing it...",
-                    self.cpu.pc
-                );
-                translator.execute(&mut self.cpu);
-                // PC is not updated?
-                self.debug_state();
-                log::debug!("after execution: pc={}", self.cpu.pc);
-            } else {
-                //log::debug!("translation block not found...");
+            if let Some(tbb) = tbb {
+                tbb.executions += 1;
 
-                // Interpret instructions normally
-                let dbb = self.interpret();
-                // log::debug!("translation block: {:?}, pc: {}", dbb, self.cpu.pc);
-
-                // Build translation block
-                let translator = TranslatorEngine::new(&llvm_context);
-                match translator.compile_dynamic_basic_block(dbb) {
-                    Ok(_) => {
-                        log::debug!("translation block successfully compiled into native code!");
-                        code_cache.put(pc, translator);
-                    }
-                    Err(e) => {
-                        log::warn!("wasn't capable to compile the translation block: {}", e);
+                if tbb.executions >= MAX_EXECUTIONS && !tbb.has_compiled() {
+                    match tbb.compile_dynamic_basic_block() {
+                        Ok(_) => {
+                            debug!("translation block successfully compiled into native code!");
+                        }
+                        Err(e) => {
+                            warn!("wasn't capable to compile the translation block: {}", e);
+                        }
                     }
                 }
+
+                if tbb.has_compiled() {
+                    debug!("executing native code...");
+                    tbb.execute(&mut self.cpu);
+                } else {
+                    let _ = self.interpret();
+                }
+
+                self.debug_state();
+
+            } else {
+
+                debug!("translation block not found...");
+
+                // Interpret instructions normally and Build translation block
+                let dbb = self.interpret();
+                let tbb = TranslationContext::new(&llvm_context, dbb);
+                code_cache.put(pc, tbb);
+
+                self.debug_state();
             }
         }
 
-        self.debug_state();
+        info!("{}", self.cpu);
 
-        println!("[info] :: {}", self.cpu);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    use crate::program::Program;
+
+    mod bytecode_gen {
+
+        use std::os::raw::{c_char, c_int};
+
+        extern "C" {
+            pub fn init(
+                buf: *mut c_char,
+                size: c_int,
+                prob: *mut c_int,
+                seed: c_int,
+                r_a: *mut c_int,
+                r_l: *mut c_int,
+            );
+        }
     }
 
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
 
+    fn generate_scenario(size: usize, seed: i32, mut probs: [i32; 5]) -> Program {
+        let mut r_a: i32 = 0;
+        let mut r_l: i32 = 0;
+
+        let mut data: Vec<u8> = Vec::new();
+        data.resize(size, 0);
+
+        unsafe {
+            bytecode_gen::init(
+                data.as_mut_ptr() as *mut i8,
+                size as i32,
+                probs.as_mut_ptr(),
+                seed,
+                &mut r_a,
+                &mut r_l,
+            )
+        }
+
+        Program::new(data, r_a, r_l)
+    }
+
+    #[test]
+    pub fn scenario_1() {
+        // acc: 30003, lc: 7
+        init();
+        let prog = generate_scenario(10_000, 1, [0, 1, 0, 0, 0]);
+        let mut vm = EmulationEngine::default();
+        vm.load_program(prog);
+        vm.main_loop();
+        assert_eq!(vm.cpu, Cpu::new(30003, 7, 10000, true));
+    }
+
+    #[test]
+    pub fn scenario_2() {
+        // acc: -1, lc: 7
+        init();
+        let prog = generate_scenario(10_000, 1, [1, 1, 1, 0, 0]);
+        let mut vm = EmulationEngine::default();
+        vm.load_program(prog);
+        vm.main_loop();
+        assert_eq!(vm.cpu, Cpu::new(-1, 7, 10000, true));
+    }
+
+    #[test]
+    pub fn scenario_3() {
+        // acc: 95, lc: -21
+        init();
+        let prog = generate_scenario(10_000, 1, [1, 9, 1, 5, 5]);
+        let mut vm = EmulationEngine::default();
+        vm.load_program(prog);
+        vm.main_loop();
+        assert_eq!(vm.cpu, Cpu::new(95, -21, 10_000, true));
+    }
+
+    #[test]
+    pub fn scenario_4() {
+        // acc: 138, lc: 0
+        init();
+        let prog = generate_scenario(50_000, 1, [1, 9, 1, 5, 5]);
+        let mut vm = EmulationEngine::default();
+        vm.load_program(prog);
+        vm.main_loop();
+        assert_eq!(vm.cpu, Cpu::new(128, 0, 50_000, true));
+    }
+
+    #[test]
+    pub fn scenario_custom_0() {
+        init();
+        let prog = Program::new(vec![2, 2, 2, 2, 2, 2, 5, 5, 0], 0, 2);
+        let mut vm = EmulationEngine::default();
+        vm.load_program(prog);
+        vm.main_loop();
+        assert_eq!(vm.cpu, Cpu::new(36, -1, 9, true));
+    }
+
+    #[test]
+    pub fn scenario_custom_1() {
+        init();
+        let prog = Program::new(vec![4, 2, 2, 2, 2, 2, 2, 2, 5, 5, 0], 0, 2);
+        let mut vm = EmulationEngine::default();
+        vm.load_program(prog);
+        vm.main_loop();
+        assert_eq!(vm.cpu, Cpu::new(21, -2, 11, true));
+    }
 }
